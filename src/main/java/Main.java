37e794180e726c81c5ee2e72ec128da2c1e1f637
ziholee <brian4574@naver.com>
2025-04-17 00:21:37 +0900

    first commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -44,7 +44,7 @@ public class Main extends ListenerAdapter {
             String playerName = event.getOption("player").getAsString();
             fetchPlayerStats(playerName, event);
         } else if (event.getName().equals("오늘의 명언")) {
-            event.reply("너 자신을 알라.\n");
+            event.reply("포기하지 않는 자가 승리를 쟁취한다.\n");
         } else if (event.getName().equals("도움말")) {
             event.reply("사용 가능한 명령어:\n" +
                     "/전적 [플레이어 이름] - 플레이어의 전적을 조회합니다.\n" +
